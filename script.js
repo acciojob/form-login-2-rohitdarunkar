5b2c3e8278921c42da8bd07f6f6004421dfd139e
Rohit Darunkar <139346362+rohitdarunkar@users.noreply.github.com>
2026-01-17 16:21:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const inputs = document.querySelectorAll("input");
+
+  let message = "";
+  inputs.forEach((input) => {
+    message += `${input.name}: ${input.value} `;
+  });
+
+  alert(message.trim());
+});
